9511c3e1930ed22d0509827ec8dd1470111065db
Chrihu03 <Chrihu03@gmail.com>
2024-03-11 18:17:14 -0700

    Initial Commit and TwoSum

--- CodingProblems/Program.cs
+++ CodingProblems/Program.cs
@@ -4,7 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] result = TwoSum.NestedLoops(new int[] {2, 7, 11, 15 }, 9);
+        Console.WriteLine("The indexes are {0} and {1}", result[0], result[1]);
+
+        result = TwoSum.NestedLoops(new int[] {3, 2, 4}, 6);
+        Console.WriteLine("The indexes are {0} and {1}", result[0], result[1]);
+
+        result = TwoSum.NestedLoops(new int[] {3, 3}, 6);
+        Console.WriteLine("The indexes are {0} and {1}", result[0], result[1]);
+
+        result = TwoSum.Dictionary(new int[] { 2, 7, 11, 15 }, 9);
+        Console.WriteLine("The indexes are {0} and {1}", result[0], result[1]);
+
+        result = TwoSum.Dictionary(new int[] { 3, 2, 4 }, 6);
+        Console.WriteLine("The indexes are {0} and {1}", result[0], result[1]);
+
+        result = TwoSum.Dictionary(new int[] { 3, 3 }, 6);
+        Console.WriteLine("The indexes are {0} and {1}", result[0], result[1]);
+
+        Console.ReadKey();
     }
 }
-
---
+++ CodingProblems/TwoSum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace CodingProblems
+{
+	public class TwoSum
+	{
+		public TwoSum()
+		{
+		}
+
+		static public int[] NestedLoops(int[] nums, int target)
+		{
+			int[] result = new int[2];
+			for (int i = 0; i < nums.Length - 1; i++)
+			{
+				for (int j = i + 1; j < nums.Length; j++)
+				{
+					if (target == (nums[i] + nums[j]))
+					{
+						result[0] = i;
+						result[1] = j;
+						i = j = nums.Length;
+					}
+				}
+			}
+			return result;
+		}
+
+        static public int[] Dictionary(int[] nums, int target)
+        {
+            int[] result = new int[2];
+			Dictionary<int, int> seenNumbers = new Dictionary<int, int>();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				int diff = target - nums[i];
+				int foundIndex;
+				if (seenNumbers.TryGetValue(diff, out foundIndex))
+				{
+					result[0] = i;
+					result[1] = foundIndex;
+					break;
+				}
+				else
+				{
+					seenNumbers.TryAdd(nums[i], i);
+				}
+			}
+            return result;
+        }
+    }
+}
+
